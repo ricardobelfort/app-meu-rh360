c45d8d3415669e66142f7a3c01a58b530c0dbef0
Ricardo Belfort <rbelfort2004@gmail.com>
2023-03-28 13:19:44 -0400

    adicionando roteamento

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CoreRoutingModule } from './core-routing.module';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    CoreRoutingModule
   ]
 })
 export class CoreModule { }
